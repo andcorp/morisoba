d4901c559684ee642b8ab8a3a8a2559c87bd1a31
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2019-09-05 17:33:30 +0900

    issue #17 Webpackでindex.tsをビルドするようにした。

--- frontend/gulpfile.ts
+++ frontend/gulpfile.ts
@@ -2,9 +2,43 @@
  * Gulpタスク定義.
  */
 
+import webpack from "webpack";
+
+/**
+ * 終了関数.
+ */
+type DoneFunction = (error?: {}) => void;
+
+// ログ等表示設定
+const stats = {
+    // ログをカラーコード付きで表示する。
+    colors: true,
+};
+
+/**
+ * ビルドタスク.
+ *
+ * @param done 終了関数
+ */
+async function build(done: DoneFunction): Promise<void> {
+    // Webpack設定を読み込む。
+    const config = (await import("./webpack.config")).default;
+
+    // 本番向けビルド設定に変更する。
+    config.mode = "production";
+    config.devtool = false;
+
+    // Webpackビルド実行
+    webpack(config).run((err, s) => {
+        // ビルド結果をログ出力する。
+        console.log(s.toString(stats));
+
+        // 処理完了。エラーがあれば報告する。
+        done(err ? err : undefined);
+    });
+}
+
 /**
  * デフォルトタスク.
  */
-export default async function defaultTask(): Promise<void> {
-    console.log("hello,world!");
-}
+export default build;
--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -2,11 +2,47 @@
  * Webpackビルド設定.
  */
 
-import { Configuration } from "webpack";
+import { Configuration, Module, Resolve, RuleSetRule } from "webpack";
 
 /**
- * Webpackデフォルト設定.
+ * ビルドルール定義.
  */
-const config: Configuration = {};
+const rules: RuleSetRule[] = [
+
+    // TypeScriptルール
+    {
+        test: /\.ts$/,
+        use: "ts-loader"
+    }
+];
+
+/**
+ * モジュール定義.
+ */
+const module: Module = { rules };
+
+/**
+ * モジュール解決方法定義.
+ */
+const resolve: Resolve = {
+
+    // 省略可能にする拡張子の設定
+    extensions: [".ts", ".js"]
+}
+
+/**
+ * Webpack設定.
+ */
+const config: Configuration = {
+
+    // アプリケーションエントリーポイント定義
+    entry: "./src/ts/index.ts",
+
+    // ソースコード解決
+    resolve,
+
+    // モジュール設定
+    module
+};
 
 export default config;

6b8726735edaf90bef3381180aeab802f8ec92be
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2019-09-05 18:11:22 +0900

    issue #17 WebpackDevServer起動まで実装した。

--- frontend/gulpfile.ts
+++ frontend/gulpfile.ts
@@ -2,7 +2,8 @@
  * Gulpタスク定義.
  */
 
-import webpack from "webpack";
+import webpack, { Configuration } from "webpack";
+import WebpackDevServer from "webpack-dev-server";
 
 /**
  * 終了関数.
@@ -16,13 +17,50 @@ const stats = {
 };
 
 /**
+ * Webpack設定を読み込む.
+ *
+ * @return Webpack設定
+ */
+async function importWebpackConfiguration(): Promise<Configuration> {
+    return (await import("./webpack.config")).default;
+}
+
+/**
+ * 開発サーバーを起動する.
+ *
+ * @return 処理結果
+ */
+export async function serve(): Promise<void> {
+    // 開発サーバー設定
+    const options: WebpackDevServer.Configuration = {
+        // 起動ホスト・ポート番号設定
+        host: "localhost",
+        port: 3000,
+
+        // ログ等の表示設定
+        stats,
+    };
+
+    // Webpack設定を読み込む。
+    const config = await importWebpackConfiguration();
+
+    // 開発サーバー向けビルド設定に変更する。
+    config.mode = "development";
+
+    // 開発サーバー起動
+    const compiler = webpack(config);
+    const server = new WebpackDevServer(compiler, options);
+    server.listen(options.port || 3000);
+}
+
+/**
  * ビルドタスク.
  *
  * @param done 終了関数
  */
-async function build(done: DoneFunction): Promise<void> {
+export async function build(done: DoneFunction): Promise<void> {
     // Webpack設定を読み込む。
-    const config = (await import("./webpack.config")).default;
+    const config = await importWebpackConfiguration();
 
     // 本番向けビルド設定に変更する。
     config.mode = "production";

c86ca9d9b87b9172b53e58d57b9bbffb0cca8381
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2020-02-19 19:24:33 +0900

    issue #35 SFC描画追加

---
+++ frontend/src/ts/components/Root.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+/**
+ * ルートコンポーネント.
+ */
+@Component
+export class Root extends Vue {
+    message = "Hello, MORISOBA";
+}
+
+export default Root;
+</script>
--- frontend/src/ts/index.ts
+++ frontend/src/ts/index.ts
@@ -2,4 +2,11 @@
  * アプリケーションのエントリーポイント.
  */
 
-console.log("MORISOBA!");
+import Vue from "vue";
+import Root from "@/components/Root.vue";
+
+// ルート要素のVueインスタンスを生成する。
+new Vue({
+    el: "#app",
+    render: (h): Vue.VNode => h(Root)
+});

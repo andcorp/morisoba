777d67dbc0d5623348e243e2f70074521fb8a2ad
a-yamaguchi <a-yamaguchi@andcorp.co.jp>
2020-02-19 18:37:51 +0900

    issue #35 フォーマット修正

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -2,12 +2,7 @@
  * Webpackビルド設定.
  */
 
-import {
-    Configuration,
-    Module,
-    Resolve,
-    RuleSetRule
-} from "webpack";
+import { Configuration, Module, Resolve, RuleSetRule } from "webpack";
 
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
@@ -16,7 +11,6 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
  * ビルドルール定義.
  */
 const rules: RuleSetRule[] = [
-
     // TypeScriptルール
     {
         test: /\.ts$/,
@@ -33,16 +27,14 @@ const module: Module = { rules };
  * モジュール解決方法定義.
  */
 const resolve: Resolve = {
-
     // 省略可能にする拡張子の設定
     extensions: [".ts", ".js"]
-}
+};
 
 /**
  * Webpack設定.
  */
 const config: Configuration = {
-
     // アプリケーションエントリーポイント定義
     entry: "./src/ts/index.ts",
 
